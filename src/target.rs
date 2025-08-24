17c5e92d08934d928b286f3d32a3fdb58ae12896
fzyier <babeckiy2004@gmail.com>
2025-08-25 00:48:00 +0300

    Add TCP probe implementation

--- src/target.rs
+++ src/target.rs
@@ -1,3 +1,7 @@
+use std::io::{Read, Write};
+use std::net::TcpStream;
+use std::time::Instant;
+
 pub trait Probe {
     fn probe(&self) -> Result<f64, String>;
 }
@@ -29,6 +33,41 @@ impl Target {
             transport,
             request: request.to_string(),
             successful_response: successful_response.to_string(),
+        }
+    }
+
+    fn probe_tcp(&self) -> Result<f64, String> {
+        let endpoint = format!("{}:{}", self.host, self.port.to_string());
+        let now = Instant::now();
+        let mut stream = TcpStream::connect(endpoint)
+            .map_err(|err| format!("Connection to {} failed with the error {}", self.host, err))?;
+
+        stream.write_all(self.request.as_bytes()).map_err(|err| {
+            format!(
+                "Failed to send the request '{}' to {} with the error {}",
+                self.request, self.host, err
+            )
+        })?;
+
+        let mut buf: String = String::new();
+        stream.read_to_string(&mut buf).map_err(|err| {
+            format!(
+                "Failed to read the {} response with the error {}",
+                self.host, err
+            )
+        })?;
+
+        let elapsed_time = now.elapsed();
+
+        Ok(elapsed_time.as_secs_f64())
+    }
+}
+
+impl Probe for Target {
+    fn probe(&self) -> Result<f64, String> {
+        match self.transport {
+            Transport::TCP => self.probe_tcp(),
+            Transport::UDP => Err("UDP is not implemented yet".to_string()),
         }
     }
 }
